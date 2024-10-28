9deb55d9b25020e4ba9b1aba1bd29df1544f1bf2
knmnltzy <knmnltzy@gmail.com>
2024-10-29 00:13:26 +0800

    add logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
 
 export default function Navbar() {
   return (
     <nav>
+        <Image
+            src={Logo}
+            alt='Dojo Helpdesk Logo'
+            width={70}
+            quality={100}
+        />
+
         <h1>Ken's Helpdesk</h1>
         <Link href="/">Dashboard</Link>
         <Link href="/tickets"> Tickets</Link>
